3aa4fb66e01cc9f298d5ea19c1bfbb765f2e27e4
vit_az <vit.azarov@gmail.com>
2022-12-27 17:07:23 -0400

    - Added models for in and out translation objects. - Added Translate client. - Added expect HttpTranslateFactory along with both Android and iOS actual implementation.

---
+++ shared/src/commonMain/kotlin/com/vitaz/pocket_translator_kmm/translate/data/translate/TranslatedDto.kt
@@ -0,0 +1,8 @@
+package com.vitaz.pocket_translator_kmm.translate.data.translate
+
+import kotlinx.serialization.SerialName
+
+@kotlinx.serialization.Serializable
+data class TranslatedDto(
+    val translatedText: String,
+)
