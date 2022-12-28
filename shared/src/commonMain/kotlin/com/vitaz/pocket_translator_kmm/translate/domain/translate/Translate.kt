df96375b4e6ed692a8bcdac5ce1061d5b0b6cae9
vit_az <vit.azarov@gmail.com>
2022-12-28 13:48:07 -0400

    - Added Translate use case for getting translation and adding the data to history DB. - Added resource wrapper class to handle API calls and errors.

---
+++ shared/src/commonMain/kotlin/com/vitaz/pocket_translator_kmm/translate/domain/translate/Translate.kt
@@ -0,0 +1,38 @@
+package com.vitaz.pocket_translator_kmm.translate.domain.translate
+
+import com.vitaz.pocket_translator_kmm.core.domain.language.Language
+import com.vitaz.pocket_translator_kmm.core.domain.util.Resource
+import com.vitaz.pocket_translator_kmm.translate.domain.history.HistoryDataSource
+import com.vitaz.pocket_translator_kmm.translate.domain.history.HistoryItem
+
+class Translate (
+    private val client: TranslateClient,
+    private val historyDataSource: HistoryDataSource
+) {
+    suspend fun execute(
+        fromLanguage: Language,
+        fromText: String,
+        toLanguage: Language
+    ): Resource<String> {
+        return try {
+            val translatedText = client.translate(
+                fromLanguage = fromLanguage,
+                fromText = fromText,
+                toLanguage = toLanguage
+            )
+            historyDataSource.insertHistoryItem(
+                HistoryItem(
+                    id = null,
+                    fromLanguageCode = fromLanguage.langCode,
+                    fromText = fromText,
+                    toLanguageCode = toLanguage.langCode,
+                    toText = translatedText
+                )
+            )
+            Resource.Success(translatedText)
+        } catch(e: TranslateException) {
+            e.printStackTrace()
+            Resource.Error(e)
+        }
+    }
+}
