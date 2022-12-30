cabab79535e4c63e11b5b782cd74cb00e904acc8
vit_az <vit.azarov@gmail.com>
2022-12-30 13:19:26 -0400

    - Added drop down and swap language components. - Set up Hilt. - Added translate screen composable. - Added navigation and translate screen to main activity.

--- androidApp/src/main/java/com/vitaz/pocket_translator_kmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/vitaz/pocket_translator_kmm/android/MainActivity.kt
@@ -6,10 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.vitaz.pocket_translator_kmm.Greeting
+import com.vitaz.pocket_translator_kmm.android.core.presentation.Routes
+import com.vitaz.pocket_translator_kmm.android.translate.presentation.AndroidTranslateViewModel
+import com.vitaz.pocket_translator_kmm.android.translate.presentation.TranslateScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +30,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    
+                    TranslateRoot()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun TranslateRoot() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Routes.TRANSLATE
+    ) {
+        composable(route = Routes.TRANSLATE) {
+            val viewModel = hiltViewModel<AndroidTranslateViewModel>()
+            val state by viewModel.state.collectAsState()
+            TranslateScreen(
+                state = state,
+                onEvent = viewModel::onEvent
+            )
         }
     }
 }
